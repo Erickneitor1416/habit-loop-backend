c3651e653c1a95c5eb584c46d46b9243bbe31023
Erick Muñoz <91401348+Erickneitor1416@users.noreply.github.com>
2024-08-04 19:12:26 -0500

    chore: add CI workflow for building, testing, and build docker for th… (#1)
    * chore: add CI workflow for building, testing, and build docker for the NestJS app
    
    * chore: fix npm build command in CI workflow
    
    * chore: update docker-compose.yml with environment variables for PORT and LOGGER_LEVEL
    
    * chore: update CI workflow to upload coverage report
    
    * chore: update CI workflow to use docker compose up command with -d flag
    
    * chore: update CI workflow to use write permissions
    
    * chore: update pull_request event in CI workflow
    
    * chore: update CI workflow to use Node.js version 20 and upload coverage report
    
    * chore: update health endpoint test in CI workflow
    
    * chore: update CI workflow to use ArtiomTr/jest
    
    * chore: update CI workflow to wait for the service and fix test cov

--- src/metadata.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-export default async () => {
-    const t = {};
-    return { "@nestjs/swagger": { "models": [], "controllers": [[import("./health/api/health.controller"), { "HealthController": { "run": {} } }]] } };
-};
